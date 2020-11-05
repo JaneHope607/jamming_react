1a43f88005ae6035d88fb09d8f1917ac300af8a6
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-11-05 14:27:15 +0000

    track.js completed

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import './Track.css';
+import React from "react";
+import "./Track.css";
 
 class Track extends React.Component {
-    
+
     renderAction() {
-        if (this.props.isRemoval) {
-            return <button className="Track-action">-</button>
-        } else {
-            return <button className="Track-action">+</button>
-        }
+        return this.props.isRemoval
+            ? <button className="Track-action">-</button>
+        : <button className="Track-action">+</button>;
     }
 
     render() {
-        return(
+        return (
             <div className="Track">
                 <div className="Track-information">
-                    <h3>{this.props.track.name}</h3>
-                        <p>{this.props.track.artist} | {this.props.track.album}</p>
+                    <h3>track name will</h3>
+                    <p>track artist  |  track album</p>
                 </div>
-                    {this.renderAction()}
+                {this.renderAction()}
             </div>
-        )
+        );
     }
 }
 
