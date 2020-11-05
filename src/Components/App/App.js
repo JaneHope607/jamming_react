a4466488d7cd438b899d09fa305c74192469f5f1
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-11-05 15:00:38 +0000

    D

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,8 +12,21 @@ class App extends React.Component {
     this.state = {
       searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1}, 
       {name: 'name2', artist: 'artist2', album: 'album2', id: 2}, 
-      {name: 'name3', artist: 'artist3', album: 'album3', id: 3}
-    ]};
+      {name: 'name3', artist: 'artist3', album: 'album3', id: 3}],
+      playlistName: "Jane's Playlist",
+      playlistTracks: [{name: 'playlistName2', artist: 'artist2', album: 'album2', id: 2}, 
+      {name: 'playlistName3', artist: 'artist3', album: 'album3', id: 3}]
+    };
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    } else {
+      tracks.push(track);
+      this.setState({playlistTracks: tracks})
+    }
   }
 
   render() {
@@ -24,7 +37,7 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} />
-            <Playlist />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
     </div>
